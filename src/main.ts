bda0fab1d41b708f9190c047a3cb03eef425dfcf
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-04-23 09:28:12 +0900

    PrimeVueのセットアップ

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,11 @@ import router from './router';
 
 const app = createApp(App);
 app.use(router);
+
+import PrimeVue from 'primevue/config';
+import "primevue/resources/themes/lara-light-indigo/theme.css";     
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
+app.use(PrimeVue);
+
 app.mount('#app');
